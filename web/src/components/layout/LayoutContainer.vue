89db3e2c18532b3e9889875e834a5e202fbcafeb
shanlihang <67490009@qq.com>
2025-01-14 22:08:32 +0800

    feat：移除示例组件，新增测试组件

---
+++ web/src/components/layout/LayoutContainer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>Welcome to your Vue.js + TypeScript app.</p>
+
+    <button>Click me</button>
+  </div>
+</template>
+
+<style scoped></style>
