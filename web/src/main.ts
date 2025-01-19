2bc22b7c14800e890e9749ef920b9c2844274d59
shanlihang <67490009@qq.com>
2025-01-19 22:06:44 +0800

    fix：移除默认样式，引入初始化操作

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,10 +4,12 @@ import "element-plus/dist/index.css";
 import "@/assets/css/reset.css";
 import App from "./App.vue";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import { systemInit } from "@/utils/init";
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 app.use(ElementPlus);
+systemInit();
 app.mount("#app");
